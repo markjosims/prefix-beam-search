40c890e5507246156bc81360b9d5c816e7a06d3f
Mark Simmons <marks21@utexas.edu>
2023-11-22 17:00:30 -0800

    move decode_audio into prefix_beam_search

--- prefix_beam_search.py
+++ prefix_beam_search.py
@@ -3,6 +3,14 @@ from string import ascii_lowercase
 import re
 import numpy as np
 
+from typing import Dict, Sequence, Union
+from transformers import pipeline, Wav2Vec2Processor
+from datasets import Dataset, Audio
+from evaluate import load
+import torch
+
+from argparse import ArgumentParser
+
 def prefix_beam_search(ctc, lm=None, k=25, alpha=0.30, beta=5, prune=0.001):
 	"""
 	Performs prefix beam search on the output of a CTC network.
@@ -82,3 +90,49 @@ def prefix_beam_search(ctc, lm=None, k=25, alpha=0.30, beta=5, prune=0.001):
 		# END: STEP 7
 
 	return A_prev[0].strip('>')
+
+pplx = load('perplexity')
+
+def wav_to_hf_audio(file: Union[str, Sequence[str]]) -> Dataset:
+    if type(file) is str:
+        file = [file]
+    audio_dict = {'audio':file}
+    ds = Dataset.from_dict(audio_dict).cast_column('audio', Audio(sampling_rate=16000))
+    return ds
+
+def decode_audio(
+        file: Union[str, Sequence[str]],
+        asr: str,
+        lm: str,
+) -> Dict[str, str]:
+    if type(file) is str:
+        file = [file,]
+    pipe = pipeline('automatic-speech-recognition', asr)
+    ctc_out = pipe(file)
+
+    lm_funct = lambda s: pplx(predictions=[s,], model_id=lm)['mean_perplexity']
+    audio_ds = wav_to_hf_audio(file)
+    processor = Wav2Vec2Processor.from_pretrained(asr)
+    def get_ctc_logits(audio: torch.tensor):
+        input_dict = processor(audio, return_tensors="pt", padding=True, sampling_rate=16000)
+        logits = asr(input_dict.input_values).logits
+        return logits
+    
+    def map_beam_search(row: dict):
+        audio = row['audio']
+        ctc_logits = get_ctc_logits(audio)
+        return {'label': prefix_beam_search(ctc_logits, lm_funct)}
+
+
+    beam_search_out = audio_ds.map(map_beam_search, remove_columns=['audio'])
+
+    return {
+        'ctc': ctc_out,
+        'beam_search': beam_search_out,
+    }
+
+if __name__ == '__main__':
+    parser = ArgumentParser('Beam search runner')
+    parser.add_argument('WAV', 'audio file to decode')
+    parser.add_argument('ASR', 'asr model path')
+    parser.add_argument('LM', 'lm model path')
